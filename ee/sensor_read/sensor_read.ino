8fc23015f786250d3ecdc510ec633bc759cc951a
W. Tyler Gee <wtylergee@gmail.com>
2013-07-27 17:43:24 -1000

    - Update to Sketch to move the temp/humidity into own sub.

--- ee/sensor_read/sensor_read.ino
+++ ee/sensor_read/sensor_read.ino
@@ -1,41 +1,43 @@
-/* Photocell simple testing sketch. 
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("Starting ee");
  
-Connect one end of the photocell to 5V, the other end to Analog 0.
-Then connect one end of a 10K resistor from Analog 0 to ground 
-Connect LED from pin 11 through a resistor to ground 
-For more information see http://learn.adafruit.com/photocells */
- 
-int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
-int photocellReading;     // the analog reading from the sensor divider
-int LEDpin = 11;          // connect Red LED to pin 11 (PWM pin)
-int LEDbrightness;        // 
-byte val = 0;
-void setup(void) {
-  // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
+  dht.begin();
 }
- 
-void loop(void) {
-  val = analogRead(photocellPin);
-  Serial.println(val,DEC);
-  adjust_led(val);
+
+void loop() {
+  read_temp_humidity();
+
   delay(100);
 }
 
-void adjust_led(int photocellReading){
-  // LED gets brighter the darker it is at the sensor
-  // that means we have to -invert- the reading from 0-1023 back to 1023-0
-  photocellReading = 1023 - int(photocellReading);
+/*
+* read_temp_humidity
+*
+* Reading temperature or humidity takes about 250 milliseconds!
+* Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+*/
+void read_temp_humidity() {
 
-  if(photocellReading < 0){
-    photocellReading = 0;
-  }
-  if(photocellReading > 250){
-    //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-    LEDbrightness = map(photocellReading, 250, 600, 0, 255);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
   } else {
-    LEDbrightness = 0;
-  }
-
-  analogWrite(LEDpin, LEDbrightness);
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }  
 }
