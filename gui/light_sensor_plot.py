a5398335568754f9dcbea0bbd116fa2fd21098d0
W. Tyler Gee <wtylergee@gmail.com>
2013-08-13 22:04:33 -1000

    -Updating of gui so it is generic rather than for light

--- gui/light_sensor_plot.py
+++ gui/light_sensor_plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import time
+import json
 import serial
 import matplotlib.pyplot as plt
 import sys
@@ -9,17 +10,17 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg \
   import FigureCanvasQTAgg as FigureCanvas
 
-import SerialData as LightSensor
+from Arduino.SerialIO import SerialData
 
-class LightMonitor(FigureCanvas):
-    """Realtime plotting of Arduino light sensor data"""
+class ArduinoSerialMonitor(FigureCanvas):
+    """Realtime plotting of Arduino serial sensor data"""
     def __init__(self):
         # initialize the iteration counter
         self.cnt = 0
         self.window_size = 30
 
-        # Get the class for getting data from light sensor
-        self.datagen = LightSensor.SerialData()
+        # Get the class for getting data from serial sensor
+        self.datagen = SerialData()
 
         self._setup_image()
 
@@ -36,39 +37,39 @@ class LightMonitor(FigureCanvas):
         self.ax.set_ylim(0, 600)
 
         # Initial empty plot
-        self.light = []
-        self.l_light, = self.ax.plot([],self.light, label='Light')
+        self.serial = []
+        self.l_light, = self.ax.plot([],self.serial, label='serial')
         self.ax.legend()
         self.fig.canvas.draw()
 
 
     def _prepare_sensor_data(self):
-        """Helper function to return light sensor info"""
+        """Helper function to return serial sensor info"""
         l_value = self.datagen.next()
+        sensor_data = json.loads(l_value)
 
-        print("l_value: {0}".format(l_value))
-        return [l_value]
+        return sensor_data.temp
 
-    def get_light(self):
-        """Get the light reading from the sensor"""
-        # take the current light sensor information
-        light_value = self._prepare_sensor_data()
+    def get_reading(self):
+        """Get the serial reading from the sensor"""
+        # take the current serial sensor information
+        serial_value = self._prepare_sensor_data()
 
-        return [light_value]
+        return [serial_value]
 
     def timerEvent(self, evt):
         """Custom timerEvent code, called at timer event receive"""
-        # get the light
-        result = self.get_light()
+        # get the serial
+        result = self.get_reading()
 
         # append new data to the datasets
-        self.light.append(result[0])
+        self.serial.append(result[0])
 
         # update lines data using the lists with new data
-        self.l_light.set_data(range(len(self.light)), self.light)
+        self.l_light.set_data(range(len(self.serial)), self.serial)
 
         # force a redraw of the Figure - we start with an initial
-        # horizaontal axes but 'scroll' as time goes by
+        # horizontal axes but 'scroll' as time goes by
         if(self.cnt >= self.window_size):
             self.ax.set_xlim(self.cnt - self.window_size, self.cnt + 15)
         self.fig.canvas.draw()
@@ -78,7 +79,7 @@ class LightMonitor(FigureCanvas):
 
 # Build and run the actual appliation
 app = QtGui.QApplication(sys.argv)
-widget = LightMonitor()
-widget.setWindowTitle("Light Sensor Data")
+widget = ArduinoSerialMonitor()
+widget.setWindowTitle("Serial Monitor")
 widget.show()
 sys.exit(app.exec_())
