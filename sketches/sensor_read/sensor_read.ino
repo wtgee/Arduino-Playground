f5a0000d75dec890fd91ff00ff8148887b39307d
W. Tyler Gee <wtylergee@gmail.com>
2013-08-15 09:43:38 -1000

    -First stab at adding photocell reading

--- sketches/sensor_read/sensor_read.ino
+++ sketches/sensor_read/sensor_read.ino
@@ -7,6 +7,10 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #include <stdarg.h>
 
+int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the sensor divider
+byte val = 0;
+
 void prints(char *fmt, ... ){
         char tmp[128]; // resulting string limited to 128 chars
         va_list args;
@@ -20,13 +24,14 @@ void setup() {
   Serial.begin(115200); 
 
   // Send initial blank values
-  prints("{\"humidity\": 0, \"temp\": 0}");
+  prints("{\"humidity\": 0, \"temp\": 0, \"photocell_0\": 0}");
  
   dht.begin();
 }
 
 void loop() {
   read_temp_humidity();
+  read_photocell(photocellPin);
 
   delay(1000);
 }
@@ -47,6 +52,23 @@ void read_temp_humidity() {
     prints("Failed to read from DHT");
   } else {
     prints("{\"humidity\": %d, \"temp\": %d}",h,t);    
+  }  
+}
 
+/*
+* read_photocell
+*
+* Reads the analog value from a photocell at given analog pin
+*/
+void read_photocell(int photocellPin) {
+  val = analogRead(photocellPin);
+
+  if (isnan(val)) {
+    prints("Failed to read from photocell %d",photocellPin);
+  } else {
+    // We want this to be a percentage of maximum (256)
+    int percent = ((val*100)/256);
+    
+    prints("{\"photocell_%d\": %d}",photocellPin, percent);
   }  
 }
